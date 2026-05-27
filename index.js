5b493bdb68ace9b782bd1aedda4bd478ef3eac5d
Ranjithbabu0912 <ranjithbabu0912@gmail.com>
2026-05-27 14:33:38 +0530

    fixed mongodb connection

--- index.js
+++ index.js
@@ -31,16 +31,24 @@ app.use("/watch", watchlaterroutes);
 app.use("/history", historyrroutes);
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  })
+  .catch((err) => {
+    console.log("MongoDB Error:", err);
+  });
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected");
 });
 
-const DBURL = process.env.MONGO_URI;
-mongoose
-  .connect(DBURL)
-  .then(() => {
-    console.log("Mongodb connected");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+mongoose.connection.on("error", (err) => {
+  console.log("Mongoose connection error:", err);
+});
